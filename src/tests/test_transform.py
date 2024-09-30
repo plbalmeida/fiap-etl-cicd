5746933d551d60e55bc1001363bf843c40204a86
plbalmeida <plbalmeida@usp.br>
2024-09-30 20:44:48 -0300

    fix(src/tests/test_transform.py): correção do import

--- src/tests/test_transform.py
+++ src/tests/test_transform.py
@@ -2,7 +2,7 @@ import pytest
 from pyspark.sql import SparkSession
 from pyspark.sql import Row
 from pyspark.sql.window import Window
-from jobs.utils import create_lag_columns
+from src.utils import create_lag_columns
 
 
 @pytest.fixture(scope="module")
