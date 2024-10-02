2832864dfcdaae3f5fbc9f315ff9f4366e7e4fab
plbalmeida <plbalmeida@usp.br>
2024-10-02 20:17:37 -0300

    fix(*): remoção no ignore do lint e ajuste no script

--- src/jobs/extract.py
+++ src/jobs/extract.py
@@ -8,6 +8,10 @@ from pyspark.context import SparkContext
 import ipeadatapy as ip
 from pyspark.sql.functions import current_date, date_format
 
+
+
+
+
 # configura o Logger
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
